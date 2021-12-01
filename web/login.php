d3fd933674f8c22690726d51403b9ea551d30ee6
Widha Astianna <widhaastianna10@gmail.com>
2021-12-01 18:37:06 +0700

    Update login.php

--- web/login.php
+++ web/login.php
@@ -1,28 +1,64 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-class Login
-{
-    //membuat sebuah fungsi yang terdapat 2 parameter, yaitu email dan password
-    function login($email, $password){
-        // menghubungkan php dengan koneksi database
-        include 'koneksi.php';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="../assets/js/jquery-3.4.1.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
-        // menyeleksi data user dengan username dan password yang sesuai
-        $login = mysqli_query($koneksi,"select * from akun where email='$email' and password='$password'");
-        // menghitung jumlah data yang ditemukan
-        $ambil = mysqli_num_rows($login);
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
 
-        // cek apakah email dan password di temukan pada database
-        if($ambil>0){
-            $data = mysqli_fetch_assoc($login);
+    <title>Document</title>
+</head>
 
-            //menampilkan kata berhasil jika logi berhasil
-            return 'berhasil';
-
-        }else{
-            //jika gagal tampil tulisan gagal
-            return 'gagal';
+<body id="login">
+    <div class="container" onsubmit="return clickLogin()">
+        <p class="p1">Selamat Datang kembali!<br>Masuk ke akun E-Majer anda dan nikmati pelayanannya</p>
+        <button class="btn-goggle"><img src="assets/gambar/icon google.png"> Lanjutkan dengan Google</button>
+        <p class="p2">Atau</p>
+        <form action="login-proses.php" method="POST">
+            <input type="text" class="input" id="nama" name="email" placeholder="Email atau Username"><br>
+            <input type="password" class="input" id="password" name="password" placeholder="password"><br>
+            <p class="forgot"><a href="">Lupa password?</a></p><br>
+            <input type="submit" value="Masuk" name="btn-login">
+            <p class="register">Belum punya akun? <a href="register.php">Daftar</a></p>
+        </form>
+    </div>
+    <script>
+        var input = document.querySelectorAll('.input');
+        for (let i = 0; i < input.length; i++) {
+            input[i].onclick = function() {
+                let j = 0;
+                while (j < input.length) {
+                    input[j++].style.outline = "none";
+                }
+                input[i].style.outline = "2px solid #FEBE10";
+            }
         }
-    }
-}
-?>
+
+        var nama = document.getElementById('nama');
+        var password = document.getElementById('password');
+
+        function clickLogin() {
+            if (nama.value == "") {
+                alert("Email atau Username Tidak Boleh Kosong");
+                return false;
+            } else if (password.value == "") {
+                alert("Password Tidak Boleh Kosong");
+                return false;
+            }
+        }
+    </script>
+
+</html>
