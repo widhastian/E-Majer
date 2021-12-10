121043faa44a38d65563ca96c5458f2607735143
ruaryvina06 <ruaryvina06@gmail.com>
2021-12-10 10:56:46 +0700

    update 10 Des Vina, transaksi

---
+++ mobile/EMajer/app/src/main/java/com/example/e_majer/databarang.java
@@ -0,0 +1,14 @@
+package com.example.e_majer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class databarang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_databarang);
+    }
+}
