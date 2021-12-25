83d20c526e6d57a8c107ec6edbb53c174c6e0f1b
Widha Astianna <widhaastianna10@gmail.com>
2021-12-25 19:32:11 +0700

    Update koneksi.php

--- web/koneksi.php
+++ web/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $server = "localhost";
-$username = "root";
-$password = "";
-$db = "emajer3";
+$username = "u1694897_b_reg_4";
+$password = "jtipolije";
+$db = "u1694897_b_reg_4_db";
 $koneksi = mysqli_connect($server, $username, $password, $db);
 
 if (mysqli_connect_errno()) {
